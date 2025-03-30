5e8bbf9c436a437693da6e96f36d3cd1331332cc
Lasxr <laserp.coding@gmail.com>
2025-03-30 16:59:46 +0700

    Update main.cpp

---
+++ AGV_Car_Platfrom/src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+
+int FF = 12;
+
+void setup() {
+
+  Serial.begin(115200);
+
+  pinMode(2, OUTPUT);
+}
+
+void loop() {
+
+  digitalWrite(2, HIGH);
+  delay(1000); 
+  digitalWrite(2, LOW);  
+  delay(1000);
+}
